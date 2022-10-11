abcf7a46fb1c3d351471c1c1c033aacc23923c10
BryanBac <49624724+BryanBac@users.noreply.github.com>
2022-10-10 23:17:04 -0600

    cambio de texto
    -Se agregó un cambio en el texto de un nuevo elemento, así como la posibilidad de modificar su color

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ function cambiar_color() {
   // Esto no es aleatorio, pero quería probar a ver si podía cambiarle el fondo xd
   if (color == 0) {
     document.getElementById("cuerpo").style.backgroundColor = "blueviolet";
-    document.getElementById("texto").innerHTML = "Background-Color: blueviolet";
+    document.getElementById("texto-color").innerHTML = "blueviolet";
+    document.getElementById("texto-color").style.color = "white";
     color = color + 1;
   } else if (color == 1) {
     document.getElementById("cuerpo").style.backgroundColor = "red";
-    document.getElementById("texto").innerHTML = "Background-Color: red";
+    document.getElementById("texto-color").innerHTML = "red";
+    document.getElementById("texto-color").style.color = "white";
     color = color + 1;
   } else if (color == 2) {
     document.getElementById("cuerpo").style.backgroundColor = "white";
-    document.getElementById("texto").innerHTML = "Background-Color: white";
+    document.getElementById("texto-color").innerHTML = "white";
+    document.getElementById("texto-color").style.color = "black";
     color = 0;
   }
   console.log("segundo:" + color);
